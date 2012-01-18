db07337c2e7400628eac7d223ebb334f32e36d14
Phillip Williams <p@bafmin.com>
2012-01-18 17:53:55 -0500

    Initial Commit of 0.2.5

---
+++ README.md
@@ -0,0 +1,99 @@
+Barcode Scanner For Appcelerator Titanium
+=========================================
+
+This is a small barcode scanner module you can use together with [Titanium Appcelerator](http://appcelerator.com) on an Android device.
+
+Build prerequisites
+-------------------
+
+To build the module, there are some dependencies which need to be fulfilled:
+
+- Git
+- Titanium Mobile SDK 1.8.0.1 or above
+- Python >= 2.5
+- Sun Java SDK 6.0
+- Android SDK with Google APIs and SDK version 8 installed
+- Ant >= 1.7.1
+
+
+Build the module
+----------------
+
+To build the module, you need to clone our git repository and build the sources with ant:
+
+    # clone the repository
+    git clone https://github.com/bafmin/Appcelerator-Android-Barcode-module
+    cd Appcelerator-Android-Barcode-module
+    # run ant to build the module
+    ant clean && ant
+
+If the build fails, make sure the entries in the buld.properies file point to the right path:
+
+    titanium.platform=/Library/Application Support/Titanium/mobilesdk/osx/1.8.0.1/android
+    android.platform=/opt/android-sdk/platforms/android-8
+    google.apis=/opt/android-sdk/add-ons/addon_google_apis_google_inc_-8
+
+The newly created *.jar and *.zip files can be found in the dist directory. The module you need is called com.mwaysolutions.barcode-android-$VERSION.zip
+
+Use the module
+--------------
+
+- Build the module or download is from the download section
+- Place it into your appcelerator project directory
+- Add the module to the module section of your tiapp.xml file: <modules><module version="0.2.5">com.mwaysolutions.barcode</module></modules>
+- Use it in your application with require('com.mwaysolutions.barcode');
+- Build your project as usual with the Titanium Developer application
+
+
+The Scanner looks strange on devices with a high resolution screen?!
+--------------------------------------------------------------------
+
+If the layout of the scanner activity is not centered, add the following entry to your tiapp.xml:
+
+    <android xmlns:android="http://schemas.android.com/apk/res/android">
+        <manifest>
+          <supports-screens
+            android:smallScreens="true"
+            android:normalScreens="true"
+            android:largeScreens="true"
+            android:anyDensity="true" />
+        </manifest>
+    </android>
+
+
+Code Example
+------------
+
+    var window = Ti.UI.createWindow({
+	    backgroundColor:'white'
+    });
+
+    var label = Ti.UI.createLabel();
+    window.add(label);
+    window.open();
+
+    var titaniumBarcode = require('com.mwaysolutions.barcode');
+
+    titaniumBarcode.scan({
+      success: function (data) {
+        if(data && data.barcode) {
+          var label = Titanium.UI.createLabel({
+            text:'Barcode: ' + data.barcode,
+            textAlign:'center',
+            width:'auto'
+          });
+          win.add(label);
+        } else {
+          alert(JSON.stringify(data));
+        }
+      },
+
+      error: function (err) { 
+        alert("Error!! " + err); 
+      },
+
+      cancel: function () { 
+        alert("cancel"); 
+      }
+    });
+
---
+++ src/com/mwaysolutions/barcode/TitaniumBarcodeModule.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2011 by M-Way Solutions GmbH
+ * 
+ *      http://www.mwaysolutions.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mwaysolutions.barcode;
+
+import org.appcelerator.kroll.KrollDict;
+import org.appcelerator.kroll.KrollInvocation;
+import org.appcelerator.kroll.KrollModule;
+import org.appcelerator.kroll.KrollProxy;
+import org.appcelerator.kroll.annotations.Kroll;
+import org.appcelerator.kroll.KrollDict;
+import org.appcelerator.kroll.KrollFunction;
+
+import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.util.Log;
+import org.appcelerator.titanium.util.TiActivityResultHandler;
+import org.appcelerator.titanium.util.TiActivitySupport;
+import org.appcelerator.titanium.util.TiIntentWrapper;
+
+
+import android.app.Activity;
+import android.content.Intent;
+
+/**
+ * This is the titanium appcelerator module which allows to use the zxing
+ * library from an appcelerator application
+ * 
+ * @author sven@roothausen.de (Sven Pfleiderer)
+ * @author p@bafmin.com (Phillip Williams) 
+ * 
+ */
+
+@Kroll.module(name = "TitaniumBarcode", id = "com.mwaysolutions.barcode")
+public class TitaniumBarcodeModule extends KrollModule {
+
+	private static final String LCAT = "TitaniumBarcodeModule";
+	private static final boolean DBG = true; // TiConfig.LOGD;
+	protected static final int UNKNOWN_ERROR = 0;
+
+	public TitaniumBarcodeModule() {
+		super();
+	}
+
+	@Kroll.method
+	public void scan(KrollDict options) {
+		logDebug("scan() called");
+
+		final KrollFunction successCallback = getCallback(options, "success");
+		final KrollFunction cancelCallback = getCallback(options, "cancel");
+		final KrollFunction errorCallback = getCallback(options, "error");
+
+		logDebug("launchScanActivity() called");
+
+		final Activity activity = TiApplication.getAppRootOrCurrentActivity();
+		final TiActivitySupport activitySupport = (TiActivitySupport) activity;
+
+		final TiIntentWrapper barcodeIntent = new TiIntentWrapper(new Intent(
+				activity, TitaniumBarcodeActivity.class));
+		barcodeIntent.setWindowId(TiIntentWrapper.createActivityName("SCANNER"));
+
+		BarcodeResultHandler resultHandler = new BarcodeResultHandler();
+		resultHandler.successCallback = successCallback;
+		resultHandler.cancelCallback = cancelCallback;
+		resultHandler.errorCallback = errorCallback;
+		resultHandler.activitySupport = activitySupport;
+		resultHandler.barcodeIntent = barcodeIntent.getIntent();
+		activity.runOnUiThread(resultHandler);
+
+		logDebug("scan() ended");
+	}
+
+	private KrollDict getDictForResult(final String result) {
+		final KrollDict dict = new KrollDict();
+		dict.put("barcode", result);
+		return dict;
+	}
+
+	private KrollFunction getCallback(final KrollDict options, final String name) {
+		if (options.containsKey(name)) {
+			return (KrollFunction) options.get(name);
+		} else {
+			logError("Callback not found: " + name);
+			return null;
+		}
+	}
+
+	private void logError(final String msg) {
+		Log.e(LCAT, msg);
+	}
+
+	private void logDebug(final String msg) {
+		if (DBG) {
+			Log.d(LCAT, msg);
+		}
+	}
+
+	protected class BarcodeResultHandler implements TiActivityResultHandler, Runnable {
+
+		protected int code;
+		protected KrollFunction successCallback, cancelCallback, errorCallback;
+		protected TiActivitySupport activitySupport;
+		protected KrollProxy proxy;
+		protected Intent barcodeIntent;
+
+		public void run() {
+			code = activitySupport.getUniqueResultCode();
+			activitySupport.launchActivityForResult(barcodeIntent, code, this);
+		}
+
+		public void onError(Activity activity, int requestCode, Exception e) {
+			String msg = "Problem with scanner; " + e.getMessage();
+			logError("error: " + msg);
+			if (errorCallback != null) {
+				errorCallback.callAsync(proxy.getKrollObject(),createErrorResponse(UNKNOWN_ERROR, msg));
+			}
+		}
+
+		public void onResult(Activity activity, int requestCode,int resultCode, Intent data) {
+			logDebug("onResult() called");
+
+			if (resultCode == Activity.RESULT_CANCELED) {
+				logDebug("scan canceled");
+				if (cancelCallback != null) {
+					cancelCallback.callAsync(proxy.getKrollObject(),new Object[0]);
+				}
+			} else {
+				logDebug("scan successful");
+				String result = data.getStringExtra(TitaniumBarcodeActivity.EXTRA_RESULT);
+				logDebug("scan result: " + result);
+				successCallback.callAsync(proxy.getKrollObject(),getDictForResult(result));
+			}
+		}
+	}
+
+}
